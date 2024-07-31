b07a36ba88e308e6dc00f501545ed2b664fadd88
VPVelthuizen <vpvelthuizen@gmail.com>
2024-07-31 11:29:49 -0400

    worked on some routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,40 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    // Find all products and include associated Category and Tag data
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    res.status(200).json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+  const product = await Product.findOne({
+    where: { id: req.params.id },
+    include: [{ model: Category }, { model: Tag }],
+  })
+
+  if(!product){
+    res.status(404).json({message: 'Product not found'});
+    return;
+  }
+} catch (err) {
+  console.error(err);
+  res.status(500).json(err);
+}
 });
 
 // create new product
